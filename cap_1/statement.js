65608a5721009e8ef4288cd635cd47434e8a4fd5
roger.silva <roger.8b@gmail.com>
2021-04-11 17:37:05 -0300

    Page 50-51 - Separating the calculation and formatting phases - StatementData Object totalAmount and totalVolume Move Function (225)

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -2,7 +2,25 @@ function statement(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformances);
+    statementData.totalAmount = totalAmount(statementData);
+    statementData.totalVolume = totalVolumeCredits(statementData);
     return renderPlainText(statementData, invoice, plays);
+
+    function totalAmount(data) {
+        let result = 0;
+        for (const perf of data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    }
+
+    function totalVolumeCredits(data) {
+        let result = 0;
+        for (const perf of data.performances) {
+            result += perf.volumeCredits;
+        }
+        return result;
+    }
 
     function enrichPerformances(aPerformance) {
         const result = Object.assign({}, aPerformance);
@@ -52,25 +70,9 @@ function statement(invoice, plays) {
         for (const perf of data.performances) {
             result += `  ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`;
         }
-        result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
-        result += `You earned ${totalVolumeCredits()} credits \n`;
+        result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
+        result += `You earned ${data.totalVolume} credits \n`;
         return result;
-
-        function totalAmount() {
-            let result = 0;
-            for (const perf of data.performances) {
-                result += perf.amount;
-            }
-            return result;
-        }
-
-        function totalVolumeCredits() {
-            let result = 0;
-            for (const perf of data.performances) {
-                result += perf.volumeCredits;
-            }
-            return result;
-        }
 
         function usd(aNumber) {
             return new Intl.NumberFormat("en-US",
