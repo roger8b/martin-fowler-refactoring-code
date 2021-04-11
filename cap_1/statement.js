92ac8b196333be87c14c669f60a5e26359f155d6
roger.silva <roger.8b@gmail.com>
2021-04-11 16:21:11 -0300

    Page 39 - Removing total credits by volume - Replace temp with query (207) Extract Function (134)

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -7,15 +7,20 @@ function statement(invoice, plays) {
         result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         totalAmount += amountFor(perf)
     }
-    
-    let volumeCredits = 0;
-    for (const perf of invoice.performances) {
-        volumeCredits = volumeCreditsFor(perf)
-    }
 
+    let volumeCredits = totalVolumeCredits();
+    
     result += `Amount owed is ${usd(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits \n`;
     return result;
+
+    function totalVolumeCredits() {
+        let volumeCredits = 0;
+        for (const perf of invoice.performances) {
+            volumeCredits = volumeCreditsFor(perf)
+        }
+        return volumeCredits;
+    }
 
     function usd(aNumber) {
         return new Intl.NumberFormat("en-US",
