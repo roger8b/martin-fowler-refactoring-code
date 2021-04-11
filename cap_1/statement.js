a3fae98ad8c3d58a77935f9df10e643d547e24e8
roger.silva <roger.8b@gmail.com>
2021-04-11 16:55:05 -0300

    Page 45-46 - Separating the calculation and formatting phases - StatementData Object add customer

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -1,9 +1,10 @@
 function statement(invoice, plays) {
     const statementData = {};
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 
     function renderPlainText(data, invoice, plays) {
-        let result = `Statement for ${invoice.customer}\n`;
+        let result = `Statement for ${data.customer}\n`;
         for (const perf of invoice.performances) {
             result += `  ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
         }
