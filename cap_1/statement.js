5a543f01e87814ad6e1e669b9f1ecaaf54e62f40
roger.silva <roger.8b@gmail.com>
2021-04-11 14:49:15 -0300

    Page 29 - Removing the play variable - Replace temp with query (207)

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -9,7 +9,7 @@ function statement(invoice, plays) {
         }).format;
 
     for (const perf of invoice.performances) {
-        const play = plays[perf.playID];
+        const play = playFor(perf);
         let thisAmount = amountFor(perf, play);
 
         // add credits for volume
@@ -24,6 +24,10 @@ function statement(invoice, plays) {
     result += `Amount owed is ${format(totalAmount / 100)}\n`;
     result += `You earned ${volumeCredits} credits \n`;
     return result;
+
+    function playFor(perf) {
+        return plays[perf.playID];
+    }
 
     function amountFor(aPerformance, play) {
         let result = 0;
