bb11ec1a6dc5fc615c4657b06c3331b50364dfda
roger.silva <roger.8b@gmail.com>
2021-04-11 14:57:18 -0300

    Page 30-31 - Removing the play variable - Change function declaration (153)

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -10,7 +10,7 @@ function statement(invoice, plays) {
 
     for (const perf of invoice.performances) {
         const play = playFor(perf);
-        let thisAmount = amountFor(perf, playFor(perf));
+        let thisAmount = amountFor(perf);
 
         // add credits for volume
         volumeCredits += Math.max(perf.audience - 30, 0);
@@ -29,10 +29,10 @@ function statement(invoice, plays) {
         return plays[perf.playID];
     }
 
-    function amountFor(aPerformance, play) {
+    function amountFor(aPerformance) {
         let result = 0;
 
-        switch (play.type) {
+        switch (playFor(aPerformance).type) {
             case "tragedy":
                 result = 40000;
                 if (aPerformance.audience > 30) {
@@ -47,7 +47,7 @@ function statement(invoice, plays) {
                 result += 300 * aPerformance.audience;
                 break;
             default:
-                throw new Error(`unknown type: ${play.type}`);
+                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
         }
 
         return result;
