cefb711dd4643fcf6c1dc81d07eb607d773b54a1
roger.silva <roger.8b@gmail.com>
2021-04-11 18:51:00 -0300

    Page 52 - Separating the calculation and formatting phases - Split statement.js

--- cap_1/statement.js
+++ cap_1/statement.js
@@ -1,65 +1,10 @@
+const { createStatementData } = require('./create-statement-data')
+
 function statement(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformances);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolume = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, invoice, plays);
 
-    function totalAmount(data) {
-        return data.performances
-        .reduce((total, p) => total + p.amount, 0);
-    }
+    return renderPlainText(createStatementData(invoice, plays));
 
-    function totalVolumeCredits(data) {
-        return data.performances
-        .reduce((total, p) => total + p.volumeCredits, 0);
-    }
-
-    function enrichPerformances(aPerformance) {
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-
-        function volumeCreditsFor(aPerformance) {
-            let result = 0;
-            result += Math.max(aPerformance.audience - 30, 0);
-            if ("comedy" == aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-            return result
-        }
-
-        function amountFor(aPerformance) {
-            let result = 0;
-
-            switch (aPerformance.play.type) {
-                case "tragedy":
-                    result = 40000;
-                    if (aPerformance.audience > 30) {
-                        result += 1000 * (aPerformance.audience - 30);
-                    }
-                    break;
-                case "comedy":
-                    result = 30000;
-                    if (aPerformance.audience > 20) {
-                        result += 10000 + 500 * (aPerformance.audience - 20);
-                    }
-                    result += 300 * aPerformance.audience;
-                    break;
-                default:
-                    throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-            }
-
-            return result;
-        }
-
-        function playFor(perf) {
-            return plays[perf.playID];
-        }
-    }
-
-    function renderPlainText(data, invoice, plays) {
+    function renderPlainText(data) {
         let result = `Statement for ${data.customer}\n`;
         for (const perf of data.performances) {
             result += `  ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`;
