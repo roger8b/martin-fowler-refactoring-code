ee34fc8c41248c9b2f73fee95c6567359690f32c
roger.silva <roger.8b@gmail.com>
2021-04-11 21:43:50 -0300

    Page 64 - Leaving the polymorphic presentation calculator - ComedyCalculator override getVolumeCredits

--- cap_1/create-statement-data.js
+++ cap_1/create-statement-data.js
@@ -52,10 +52,7 @@ class PerformanceCalculator {
     }
 
     get volumeCredit() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        if ("comedy" == this.play.type) result += Math.floor(this.performance.audience / 5);
-        return result
+        return Math.max(this.performance.audience - 30, 0);
     }
 }
 
@@ -80,6 +77,10 @@ class ComedyCalculator extends PerformanceCalculator {
         result += 300 * this.performance.audience;
 
         return result
+    }
+
+    get volumeCredit() {
+        return Math.floor(this.performance.audience / 5);
     }
 
 }
